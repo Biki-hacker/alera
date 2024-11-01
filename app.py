7d46f450e17a6b9fe119b26c9b4e9c8542ae5e2c
Biki-hacker <dilip.dhara74@gmail.com>
2024-11-01 12:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import re
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -7,7 +8,6 @@ app = Flask(__name__)
 CORS(app)
 
 def extract_video_id(youtube_url):
-    
     video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11}).*", youtube_url)
     if video_id_match:
         return video_id_match.group(1)
@@ -16,23 +16,18 @@ def extract_video_id(youtube_url):
 
 @app.route('/transcript', methods=['POST'])
 def get_youtube_transcript():
-    
     data = request.get_json()
     youtube_url = data.get('url')
     try:
-        
         video_id = extract_video_id(youtube_url)
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
         
-       
         cleaned_text = " ".join([entry['text'].replace('\n', ' ') for entry in transcript])
-        
-        
         return jsonify({"transcript": cleaned_text}), 200
     except Exception as e:
-        
         return jsonify({"error": str(e)}), 400
 
-
 if __name__ == '__main__':
-    app.run()
+    # Set host to 0.0.0.0 and port to the PORT environment variable or default to 10000
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
